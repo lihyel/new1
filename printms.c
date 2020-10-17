af0fd2cde509d62fc1d007fa1a66b2746ba612a4
lihyel <68941380+lihyel@users.noreply.github.com>
2020-10-17 13:06:34 +0900

    Add files via upload

--- printms.c
+++ printms.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-void hello(char p, char male) {
-	char p;
-	printf("이름을 입력하세요.");
-	scnaf("%c", &p);
+void hello(char *p, char male);
+
+int main() {
+	char name[10], male;
+	printf("이름을 입력하세요.\n");
+	scanf_s("%s", name, sizeof name); //&s:문자열(string)
+	getchar();
+	printf("성별을 입력하세요(m/f).\n");
+	scanf_s("%c", &male);
 	
-	char male;
-	printf("성별을 입력하세요(m/f).");
-	scanf("%c", &male);
+	hello(name, male); 
 
-	if (male == 'm') {
-		printf("Hello Mr.%c", &p);
-	}
-	else if (male == 'f') {
-		printf("Hello Ms.%c", &p);
-	}
-	else {
-		printf("Hello %c", &p);
-	}
+	return 0;
 }
 
-int main(void) {
-	hello;
+void hello(char *p, char male) {
 
-	return 0; 
+	if (male=='m') 
+		printf("Hello Mr.%s\n", p);
+	else if (male=='f')
+		printf("Hello Ms.%s\n", p);
+	else 
+		printf("Hello %s\n", p); //*p아님 주의, %s로 입력받았으니 문자열로 출력해줘야함(%c아님)
 }
+
